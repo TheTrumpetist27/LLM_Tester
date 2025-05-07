8fa715fca6adb9e8538e35ef84619b028b803129
Kian Bruyenberg <kbruyenberg@gmail.com>
2025-05-07 15:49:45 +0200

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def read_sample(file_path):
         return file.read()
 
 def save_summary(text, model_name):
-    output_file = f"results/samenvatting_{model_name}2.txt"
+    output_file = f"results/samenvatting_{model_name}_5.txt"
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(text)
     print(f"Samenvatting opgeslagen in {output_file}")
--- models/t5_model.py
+++ models/t5_model.py
@@ -7,13 +7,15 @@ class T5Summarizer:
         self.model = T5ForConditionalGeneration.from_pretrained(model_name)
 
     def summarize(self, text, max_length=150, min_length=40):
-        input_text = "Please write a summary of the following article text: " + text.strip().replace("\n", " ")
-        inputs = self.tokenizer(input_text, return_tensors="pt", max_length=512, truncation=True)
+        input_text = "Please write a summary of the following article text while capturing key points: " + text.strip().replace("\n", " ")
+        inputs = self.tokenizer(input_text, return_tensors="pt", max_length=1024, truncation=True)
+        print(len(inputs))
         
         summary_ids = self.model.generate(inputs["input_ids"], 
                                           max_length=max_length, 
                                           min_length=min_length, 
-                                          length_penalty=2.0, 
-                                          num_beams=4, 
-                                          early_stopping=True)
+                                          length_penalty=1.0, 
+                                          num_beams=8,
+                                          do_sample=False,
+                                          early_stopping=False)
         return self.tokenizer.decode(summary_ids[0], skip_special_tokens=True)
