1548b16c20c1ad754e7e8e1643563d9ad5defecc
Kian Bruyenberg <kbruyenberg@gmail.com>
2025-06-04 14:58:05 +0200

    Update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def read_sample(file_path):
         return file.read()
 
 def save_summary(text, model_name):
-    output_file = f"voorbeeldTweeResults/samenvatting_{model_name}_1.txt"
+    output_file = f"voorbeeldTweeResults/samenvatting_{model_name}_2.txt"
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(text)
     print(f"Samenvatting opgeslagen in {output_file}")
--- models/bart_model.py
+++ models/bart_model.py
@@ -5,7 +5,7 @@ class BARTSummarizer:
         self.tokenizer = BartTokenizer.from_pretrained(model_name)
         self.model = BartForConditionalGeneration.from_pretrained(model_name)
 
-    def summarize(self, text, max_length=150, min_length=40):
+    def summarize(self, text, max_length=140, min_length=40):
         inputs = self.tokenizer(text, return_tensors="pt", max_length=1024, truncation=True)
         print(f"Aantal tokens: {len(inputs['input_ids'][0])}")
         summary_ids = self.model.generate(inputs["input_ids"], 
